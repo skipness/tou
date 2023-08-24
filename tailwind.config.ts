868e7e5ab09154d0eaa8dca3ffff4e86dcffd29b
skipness <ckhf@proton.me>
2023-08-24 12:31:56 -0400

    Remove unused style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,20 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      // backgroundImage: {
-      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-      //   'gradient-conic':
-      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      // },
       colors: {
         gold: '#ffd700',
         lightgrey: '#d3d3d3',
-        secondary: '#1e1e1e'
       },
       fontSize: {
         '9xl': '7rem',
         '10xl': '8rem',
-        '14xl': '12rem',
       },
     },
   },
